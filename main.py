998cf08fad803117ad9ab400f9afd90016d92493
Xnoppanat <nine.noppanat@gmail.com>
2023-06-04 21:21:39 +0700

    Completed task

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import sys
-from functions import add, sub , mul , div
+from functions import add, sub , mul , div , power
 
+arr = ['main.py','1','2','3']
 n1 = int(sys.argv[1])
 operator = sys.argv[2]
 n2 = int(sys.argv[3]) 
@@ -13,5 +14,7 @@ if operator == 'x':
     r = mul(n1,n2)
 if operator == '/':
     r = div(n1,n2)
+if operator == 'pw':
+    r = power(n1,n2)
 
 print r 
