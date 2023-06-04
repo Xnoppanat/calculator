52870c2a0c942b999340b17dd9a23472a60ac8bd
Xnoppanat <nine.noppanat@gmail.com>
2023-06-04 20:58:45 +0700

    Completed Calculator

--- functions.py
+++ functions.py
@@ -8,5 +8,5 @@ def sub(a,b):
 def mul(a,b):
     return a * b
 
-def dev(a,b):
+def div(a,b):
     return a / b
--- main.py
+++ main.py
@@ -1,1 +1,17 @@
+import sys
 from functions import add, sub , mul , div
+
+n1 = int(sys.argv[1])
+operator = sys.argv[2]
+n2 = int(sys.argv[3]) 
+
+if operator == '+':
+    r = add(n1,n2)
+if operator == '-':
+    r = sub(n1,n2)
+if operator == 'x':
+    r = mul(n1,n2)
+if operator == '/':
+    r = div(n1,n2)
+
+print r 
